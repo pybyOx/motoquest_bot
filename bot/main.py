7f411022f1da8f61f56c6aaf89c91543395f3a85
Oksana Ushakova <oksanchik123456@mail.ru>
2026-01-16 16:53:17 +0700

    chore: update main.py bootstrap

--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,16 @@
 import utils.misc.logger
 import logging
 from loader import bot
-import handlers  # noqa
+import handlers  # noqa: F401  # регистрация хендлеров
 from database.database_model import create_models, setup_db, close_db
 from telebot.custom_filters import StateFilter
 import atexit
 
 
 if __name__ == "__main__":
-    logging.debug("Начало")
+    logging.debug("Bot starting")
     setup_db()
     create_models()
     bot.add_custom_filter(StateFilter(bot))
     atexit.register(close_db)
     bot.infinity_polling()
-
